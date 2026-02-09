dfc1f38ca2ef40addf2d3d8f54ff44367d0da2fb
abhishektiwari-maker <abhishek.tiwari@kalvium.community>
2026-02-09 11:35:21 +0530

    name endpoint

--- index.js
+++ index.js
@@ -4,5 +4,7 @@ const app = express();
 
 app.get("/",(req,res)=>res.send("This is deployed backend"))
 
+app.get("/:name",(req,res)=>res.send(`Hello ${req.params.name}`));
+
 
 app.listen(3500,()=>console.log("Server is running on port 3500"))
